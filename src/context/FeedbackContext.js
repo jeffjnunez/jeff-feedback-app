d18c392bd162afdebf208abc745cbb87c326e364
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-03-14 12:54:53 -0700

    commented out any fetch calls, now using fake data re-added uuid; removed json-server, concurrently

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,17 +1,21 @@
 import { createContext, useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import FeedbackData from '../data/FeedbackData';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
     const [isLoading, setIsLoading] = useState(true);
-    const [feedback, setFeedback] = useState([]);
+    const [feedback, setFeedback] = useState(FeedbackData);
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false,
     });
 
+    // Not using persistent data for the vercel-deployment branch.
     useEffect(() => {
-        fetchFeedback();
+        // fetchFeedback();
+        setIsLoading(false);
     }, []);
 
     const fetchFeedback = async () => {
@@ -22,19 +26,24 @@ export const FeedbackProvider = ({ children }) => {
         setIsLoading(false);
     };
 
-    const addFeedback = async (newFeedback) => {
-        const response = await fetch('/feedback', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newFeedback)
-        });
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback, ...feedback]);
+    }
 
-        const data = await response.json();
+    // const addFeedback = async (newFeedback) => {
+    //     const response = await fetch('/feedback', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(newFeedback)
+    //     });
 
-        setFeedback([data, ...feedback]);
-    };
+    //     const data = await response.json();
+
+    //     setFeedback([data, ...feedback]);
+    // };
 
     // Set item to be updated.
     const editFeedback = (item) => {
@@ -44,25 +53,31 @@ export const FeedbackProvider = ({ children }) => {
         });
     };
 
-    const updateFeedback = async (id, updatedItem) => {
-        const response = await fetch(`/feedback/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(updatedItem),
-        });
-
-        const data = await response.json();
-
+    const updateFeedback = (id, updatedItem) => {
         setFeedback(feedback.map((item) => {
-            return item.id === id ? { ...item, ...data } : item;
+            return item.id === id ? { ...item, ...updatedItem } : item;
         }));
-    };
+    }
+
+    // const updateFeedback = async (id, updatedItem) => {
+    //     const response = await fetch(`/feedback/${id}`, {
+    //         method: 'PUT',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(updatedItem),
+    //     });
+
+    //     const data = await response.json();
+
+    //     setFeedback(feedback.map((item) => {
+    //         return item.id === id ? { ...item, ...data } : item;
+    //     }));
+    // };
 
     const deleteFeedback = async (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
-            await fetch(`/feedback/${id}`, { method: 'DELETE' });
+            // await fetch(`/feedback/${id}`, { method: 'DELETE' });
 
             setFeedback(feedback.filter((item) => item.id !== id));
         }
