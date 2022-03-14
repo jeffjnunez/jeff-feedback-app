0f752d8cbc4eba8d933fe7d05a2a7660c23e35d4
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-03-14 12:36:41 -0700

    re-added static sample data file

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: '1',
+        rating: 10,
+        text: 'Sample feedback item 1',
+    },
+    {
+        id: '2',
+        rating: 9,
+        text: 'Sample feedback item 2 with a slightly longer text value.',
+    },
+    {
+        id: '3',
+        rating: 8,
+        text: 'Sample feedback item 3 with a significantly longer text value. Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+];
+
+export default FeedbackData;
